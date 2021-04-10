76f261e55e067a0c28e741318865f2666328b3c8
Richard Walmsley <richwalm@gmail.com>
2021-04-10 16:50:04 +1200

    Handles deletions and improved the code.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask, request, abort
+import logging
 import os
 import hmac
 import urllib.request
 import json
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 if 'UPBANK_PAT' not in os.environ:
     raise Exception('You need to define UPBANK_PAT.')
@@ -15,23 +17,77 @@ if 'FIREFLY_PAT' not in os.environ:
     raise Exception('You need to define FIREFLY_PAT.')
 if 'FIREFLY_BASEURL' not in os.environ:
     raise Exception('You need to define FIREFLY_BASEURL.')
+
 Timeout = 10
 
+def PerformRequest(URL, PAT, Accept = None, Method = None, IsJSON = False):
+    try:
+        Req = urllib.request.Request(URL, method = Method)
+        Req.add_header('Authorization', 'Bearer {}'.format(PAT))
+        if Accept:
+            Req.add_header('Accept', Accept)
+        Resp = urllib.request.urlopen(Req, timeout = Timeout)
+        if IsJSON:
+            Data = json.load(Resp)
+        else:
+            Data = Resp.read()
+    except urllib.error.HTTPError as e:
+        app.logger.exception('Got HTTP status code %s while downloading; %s', e.code, URL)
+        return None, True
+    except json.JSONDecodeError:
+        app.logger.exception('Expected JSON is not valid; %s', URL)
+        return None, True
+    except Exception:
+        app.logger.exception('Failed to download JSON; %s', URL)
+        return None, False
+
+    return Data, None
+
 def DeleteTransaction(ID):
-    app.logger.info('Received a delete message for ID; {}'.format(ID))
-    # TODO: Search for the transaction.
-    # TODO: Delete it.
-    pass
+    app.logger.info('Received a delete message for ID; %s', ID)
+
+    # Search for the Up ID in Firefly.
+    URL = '{}/api/v1/search/transactions?query=external_id:{}'.format(os.environ['FIREFLY_BASEURL'], ID)
+    Data = PerformRequest(URL, os.environ['FIREFLY_PAT'], 'application/vnd.api+json', IsJSON = True)
+    if not Data[0]:
+        return False
+
+    JSON = Data[0]
+
+    try:
+        Count = len(JSON['data'])
+    except Exception:
+        app.logger.error('Unexpected JSON from Firefly\'s URL; %s', URL)
+        return False
+
+    if not Count:
+        app.logger.warning('No Firefly transaction with the external_id of %s.', ID)
+        return False
+    elif Count > 1:
+        # Should be in most recent order so this shouldn't matter.
+        app.logger.warning('Multiple transactions with the external_id of %s. Using the first.', ID)
+
+    try:
+        FireflyID = JSON['data'][0]['id']
+    except Exception:
+        app.logger.exception('Failed to extract ID from Firefly\'s search results for external_id; %s', ID)
+        return False
+
+    # Delete from Firefly.
+    URL = '{}/api/v1/transaction/{}'.format(os.environ['FIREFLY_BASEURL'], FireflyID)
+    Data = PerformRequest(URL, os.environ['FIREFLY_PAT'], None, 'DELETE')
+    if not Data[0]:
+        return False
+
+    app.logger.info('Successfully deleted transaction %s (Up ID %s)', FireflyID, ID)
+    return True
 
 def HandleTransaction(Type, TransactionData):
-    app.logger.info('Received a {} message to process.'.format(Type))
-    # TODO: Search for the transaction.
-    # TODO: Update in Firefly.
-    pass
+    app.logger.info('Received a %s message to process.', Type)
 
-@app.route('/', methods = ['POST'])
-def index():
-    # First off, check to ensure that this is from Up Bank.
+    return False
+
+def CheckMessageSecure():
     AuthHeader = request.headers.get('X-Up-Authenticity-Signature')
     if not AuthHeader:
         app.logger.warn('Missing X-Up-Authenticity-Signature header.')
@@ -47,6 +103,10 @@ def index():
     if not hmac.compare_digest(Digest, AuthHeader):
         app.logger.error('HMAC did\'t match; %s != %s', Digest, AuthHeader)
         abort(403)
+
+@app.route('/', methods = ['POST'])
+def index():
+    CheckMessageSecure()
 
     # Valid message. Convert to JSON.
     JSON = request.get_json(cache = False)
@@ -70,33 +130,22 @@ def index():
         app.logger.info('Received a ping message.')
         return 'PONG'
 
-    elif Type in {'TRANSACTION_CREATED', 'TRANSACTION_CREATED'}:
+    elif Type in {'TRANSACTION_CREATED', 'TRANSACTION_SETTLED'}:
         try:
             TransactionURL = JSON['data']['relationships']['transaction']['links']['related']
         except Exception:
-            app.logger.exception('{} payload missing transaction URL.'.format(Type))
+            app.logger.exception('%s payload missing transaction URL.', Type)
             abort(400)
 
-        # Download URL & convert to JSON.
+        Data = PerformRequest(TransactionURL, os.environ['UPBANK_PAT'], IsJSON = True)
+        if not Data[0]:
+            abort(400 if Data[1] else 500)
+
+        # Since we've gotten initial valid data from Up, return success from this point forward.
         try:
-            Req = urllib.request.Request(TransactionURL)
-            Req.add_header('Authorization', 'Bearer {}'.format(os.environ['UPBANK_PAT']))
-            Resp = urllib.request.urlopen(Req, timeout = Timeout)
-            JSONResp = json.load(Resp)
-
-        except urllib.error.HTTPError as e:
-            app.logger.exception('HTTP error while downloading transaction.')
-            if e.code == 404:
-                abort(400)
-            abort(500)
-        except json.JSONDecodeError:
-            app.logger.exception('Transaction JSON is not valid.')
-            abort(400)
+            HandleTransaction(Type, Data[0])
         except Exception:
-            app.logger.exception('Failed to download transaction.')
-            abort(500)
-
-        HandleTransaction(Type, JSONResp)
+            app.logger.exception('Failed while processing %s transaction.', Type)
 
     elif Type == 'TRANSACTION_DELETED':
         try:
@@ -105,10 +154,14 @@ def index():
             app.logger.exception('Delete payload missing transaction ID.')
             abort(400)
 
-        DeleteTransaction(TransactionID)
+        # Since we've gotten initial valid data from Up, return success from this point forward.
+        try:
+            DeleteTransaction(TransactionID)
+        except Exception:
+            app.logger.exception('Failed while processing delete transaction for %s.', TransactionID)
 
     else:
-        app.logger.error('Unexpected resource event type; {}'.format(Type))
+        app.logger.error('Unexpected resource event type; %s', Type)
         abort(400)
 
     return 'THANKS'
